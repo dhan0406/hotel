f89309e0b2e814df4bae99594e283e736892bb88
Diana Han <dianah46@gmail.com>
2019-09-07 14:41:30 -0700

    created #make_reservation, #list_reservations, #list_available_rooms methods in BookingSystem class and its unit tests

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -1,2 +1,54 @@
+require_relative 'date_range.rb'
+require_relative 'reservation.rb'
+require 'date'
+
 class BookingSystem
+  attr_reader :reservations, :rooms
+  
+  def initialize
+    @reservations = []
+    @rooms = [*1..20]
+  end
+
+  # given the valid date range, can create an instance of reservation by assigning first available room to reservation
+  
+
+  def make_reservation(room, date_range)
+    reservation = Reservation.new(room: room, date_range: date_range)
+    @reservations << reservation
+
+
+    # if 'room' is not included in the list of 'available rooms', raise an error
+  else
+    # create a reservation with given
+
+  
+
+  end
+
+  def find_available_room(date_range)
+    # if #date_range.overlap? returns false, create a Reservation by assigning a room 
+     
+  end
+
+  def list_reservations(date)
+    @reservations.each do |reservation|
+      if reservation.date_range.range.include?(date)
+        return reservation
+      end
+    end
+  end
+
+  def list_available_rooms(date_range)
+    available_rooms = rooms.clone
+
+    @reservations.each do |reservation|
+      unless ((date_range.out_date <= reservation.date_range.in_date || date_range.in_date >= reservation.date_range.out_date))
+
+      available_rooms.delete(reservation.room)
+      end
+      
+      return available_rooms
+    end
+  end
 end
--- test/booking_system_test.rb
+++ test/booking_system_test.rb
@@ -1,1 +1,64 @@
 require_relative 'test_helper'
+require 'pry'
+
+describe "BookingSystem class" do
+  describe "#initialze method" do
+    it "can create an instance of BookingSystem" do
+    @new_booking_system = BookingSystem.new
+
+    expect(@new_booking_system).must_be_instance_of BookingSystem
+    end
+  end
+
+  describe "#make_reservation method" do
+    it "can create a reservation given valid dates" do
+      @new_booking_system = BookingSystem.new
+
+      @new_booking_system.make_reservation(1, DateRange.new('2019-12-01', '2019-12-04'))
+
+      expect(@new_booking_system.reservations[0]).must_be_instance_of Reservation
+    end
+
+    it "will raise an error if no rooms are available for reservation" do
+    end
+
+  end
+
+  describe "#list_reservations method" do
+   
+    it "can return a list of reservations that include the given date" do
+      @new_booking_system = BookingSystem.new
+      @new_booking_system.make_reservation(1, DateRange.new('2019-12-01', '2019-12-04'))
+
+      date = Date.parse('2019-12-04')
+
+      reservation = @new_booking_system.list_reservations(date)
+
+      expect(reservation.room).must_equal 1
+    end
+  end
+
+  describe "#list_available_rooms method" do
+    before do
+      @new_booking_system = BookingSystem.new
+
+      @new_booking_system.make_reservation(1, DateRange.new('2019-12-01', '2019-12-04'))
+    end
+
+    it "can return a list of available rooms" do
+      date_range_one = DateRange.new('2019-12-04', '2019-12-09')
+      date_range_two = DateRange.new('2019-11-30', '2019-12-01')
+
+      expect(@new_booking_system.list_available_rooms(date_range_one)).must_equal [*1..20]
+      expect(@new_booking_system.list_available_rooms(date_range_two)).must_equal [*1..20]
+    end
+
+    it "room one should not be available if dates conflict" do
+      date_range = DateRange.new('2019-12-01', '2019-12-02')
+
+      expect(@new_booking_system.list_available_rooms(date_range)).must_equal [*2..20]
+    end
+
+  end
+
+end
